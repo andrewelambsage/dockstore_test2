bee56cf0c2c01e6558842f7adb2bf59a463bb13d
Andrew Lamb <andrew.lamb@sagebase.org>
2017-08-23 12:08:04 -0700

    add arg

--- set_seed.R
+++ set_seed.R
@@ -5,10 +5,13 @@ library(argparse)
 
 parser = ArgumentParser(description='Generate diploid TPM files for read simulation.')
 parser$add_argument('--seed', default = NULL,  type="integer", help = "Seed number to use for RSEM read simulation.")
+parser$add_argument('--output', default = "./output.txt",  type="character")
 args = parser$parse_args()
 
 # set seed if seed argument is numeric
 if(is.numeric(args$seed)) {
   set.seed(args$seed)
 }
+
+writeLines(as.character(args$seed), args$output)
 
